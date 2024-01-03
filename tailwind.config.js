259d9791e205be9aede40af0b6223ba9940b7320
Demira11 <117664846+Demira11@users.noreply.github.com>
2024-01-03 18:32:43 -0500

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: ["./build/*.html"],
+  darkMode: "class", // Enable dark mode with the class variant
   theme: {
     extend: {},
   },
